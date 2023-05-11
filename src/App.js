73daa1579115c4f3e49730702287144bc17ad657
mahender011999 <mahender011999@gmail.com>
2023-05-11 18:45:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import UserProfile from "./components/Userprofile"
+import './App.css'
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Mahender',
+    role: 'Full Stack Developer'
+  },
+  {
+    uniqueNo: 2,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'magana',
+    role: 'Application Developer'
+  },
+  {
+    uniqueNo: 3,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'uma',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 4,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'ravali',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 5,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Tonny',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 6,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 7,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 8,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer'
+  }
+]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map((eachItem) => (
+        <UserProfile userDetails={eachItem} />
+      ))}
+    </ul>
+  </div>
+)
+export default App
